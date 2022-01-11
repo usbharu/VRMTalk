806691d909746af47cb86c5dfb94f45313233bed
usbharu <64310155+usbharu@users.noreply.github.com>
2022-01-12 00:44:16 +0900

    テストを追加

---
+++ Assets/VRMTalk/Tests/VRMTalk/VRMTalkTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using VRM;
+using VRMTalk.Editor;
+using Assert = UnityEngine.Assertions.Assert; 
+
+namespace VRMTalk
+{
+    public class VRMTalkTests
+    {
+       
+        [Test]
+        public void GetBlendShapeNames_UseTestModel_IsNotNull()
+        {
+            var vrm = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/VRMTalk/Tests/VRMTalk/Resources/TestModel.prefab");
+            var vrmMeta = vrm.GetComponent<VRMMeta>();
+            Assert.IsNotNull(VRMTalkUtility.GetBlendShapeNames(vrmMeta));
+        }
+        
+        [Test]
+        public void CharacterUnification_SimpleCharacter_IsHiraganaChar()
+        {
+            Assert.AreEqual(VRMTalkUtility.CharacterUnification('ア'),'あ');
+            Assert.AreEqual(VRMTalkUtility.CharacterUnification('ﾜ'),'わ');
+            Assert.AreEqual(VRMTalkUtility.CharacterUnification('ヱ'),'ゑ');
+        }
+    }
+}
